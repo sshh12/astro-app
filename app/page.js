bbeb3a6a6b4cdfb61defa4cac1bb503641b4defd
Shrivu Shankar <shrivu1122@gmail.com>
2024-02-02 17:24:49 -0800

    tweaks

--- app/page.js
+++ app/page.js
@@ -187,49 +187,91 @@ export default function Home() {
     yAxisWidth: 60,
   };
   return (
-    <main
-      style={{ padding: "0px 10px 10px 10px" }}
-      className="bg-tremor-background"
-    >
-      <div
-        className="sticky-top bg-tremor-background"
-        style={{ padding: "16px 0px 16px 0px" }}
-      >
-        <Title>Dashboard</Title>
-        <Text>Lorem ipsum dolor sit amet, consetetur sadipscing elitr.</Text>
-      </div>
+    <>
+      <main style={{ padding: "0px 10px 5rem 10px" }} className="bg-slate-800">
+        <div
+          className="sticky-top bg-slate-800"
+          style={{ padding: "16px 0px 16px 0px" }}
+        >
+          <Title>Dashboard</Title>
+          <Text>Lorem ipsum dolor sit amet, consetetur sadipscing elitr.</Text>
+        </div>
 
-      <TabGroup className="mt-6">
-        <TabList>
-          <Tab>Overview</Tab>
-          <Tab>Detail</Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel>
-            <Grid numItemsMd={2} numItemsLg={3} className="mt-6 gap-6">
-              {kpiData.map((item) => (
-                <Card key={item.title}>
-                  <Flex alignItems="start">
-                    <div className="truncate">
-                      <Text>{item.title}</Text>
-                      <Metric className="truncate">{item.metric}</Metric>
+        <TabGroup className="mt-6">
+          <TabList>
+            <Tab>Overview</Tab>
+            <Tab>Detail</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Grid numItemsMd={2} numItemsLg={3} className="mt-6 gap-6">
+                {kpiData.map((item) => (
+                  <Card key={item.title}>
+                    <Flex alignItems="start">
+                      <div className="truncate">
+                        <Text color="white">{item.title}</Text>
+                        <Metric className="truncate">{item.metric}</Metric>
+                      </div>
+                      <BadgeDelta deltaType={item.deltaType}>
+                        {item.delta}
+                      </BadgeDelta>
+                    </Flex>
+                    <Flex className="mt-4 space-x-2">
+                      <Text className="truncate">{`${item.progress}% (${item.metric})`}</Text>
+                      <Text className="truncate">{item.target}</Text>
+                    </Flex>
+                    <ProgressBar value={item.progress} className="mt-2" />
+                  </Card>
+                ))}
+              </Grid>
+              <div className="mt-6">
+                <Card>
+                  <>
+                    <div className="md:flex justify-between">
+                      <div>
+                        <Flex
+                          className="space-x-0.5"
+                          justifyContent="start"
+                          alignItems="center"
+                        >
+                          <Title> Performance History </Title>
+                          <Icon
+                            icon={InformationCircleIcon}
+                            variant="simple"
+                            tooltip="Shows daily increase or decrease of particular domain"
+                          />
+                        </Flex>
+                        <Text> Daily change per domain </Text>
+                      </div>
+                      <div>
+                        <TabGroup
+                          index={selectedIndex}
+                          onIndexChange={setSelectedIndex}
+                        >
+                          <TabList color="gray" variant="solid">
+                            <Tab>Sales</Tab>
+                            <Tab>Profit</Tab>
+                            <Tab>Customers</Tab>
+                          </TabList>
+                        </TabGroup>
+                      </div>
                     </div>
-                    <BadgeDelta deltaType={item.deltaType}>
-                      {item.delta}
-                    </BadgeDelta>
-                  </Flex>
-                  <Flex className="mt-4 space-x-2">
-                    <Text className="truncate">{`${item.progress}% (${item.metric})`}</Text>
-                    <Text className="truncate">{item.target}</Text>
-                  </Flex>
-                  <ProgressBar value={item.progress} className="mt-2" />
+                    {/* web */}
+                    <div className="mt-8 hidden sm:block">
+                      <LineChartExample />
+                    </div>
+                    {/* mobile */}
+                    <div className="mt-8 sm:hidden">
+                      <LineChartExample />
+                    </div>
+                  </>
                 </Card>
-              ))}
-            </Grid>
-            <div className="mt-6">
-              <Card>
-                <>
-                  <div className="md:flex justify-between">
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className="mt-6">
+                <Card>
+                  <>
                     <div>
                       <Flex
                         className="space-x-0.5"
@@ -240,145 +282,110 @@ export default function Home() {
                         <Icon
                           icon={InformationCircleIcon}
                           variant="simple"
-                          tooltip="Shows daily increase or decrease of particular domain"
+                          tooltip="Shows sales performance per employee"
                         />
                       </Flex>
-                      <Text> Daily change per domain </Text>
                     </div>
-                    <div>
-                      <TabGroup
-                        index={selectedIndex}
-                        onIndexChange={setSelectedIndex}
+                    <div className="flex space-x-2">
+                      <MultiSelect
+                        className="max-w-full sm:max-w-xs"
+                        onValueChange={setSelectedNames}
+                        placeholder="Select Salespeople..."
                       >
-                        <TabList color="gray" variant="solid">
-                          <Tab>Sales</Tab>
-                          <Tab>Profit</Tab>
-                          <Tab>Customers</Tab>
-                        </TabList>
-                      </TabGroup>
-                    </div>
-                  </div>
-                  {/* web */}
-                  <div className="mt-8 hidden sm:block">
-                    <LineChartExample />
-                  </div>
-                  {/* mobile */}
-                  <div className="mt-8 sm:hidden">
-                    <LineChartExample />
-                  </div>
-                </>
-              </Card>
-            </div>
-          </TabPanel>
-          <TabPanel>
-            <div className="mt-6">
-              <Card>
-                <>
-                  <div>
-                    <Flex
-                      className="space-x-0.5"
-                      justifyContent="start"
-                      alignItems="center"
-                    >
-                      <Title> Performance History </Title>
-                      <Icon
-                        icon={InformationCircleIcon}
-                        variant="simple"
-                        tooltip="Shows sales performance per employee"
-                      />
-                    </Flex>
-                  </div>
-                  <div className="flex space-x-2">
-                    <MultiSelect
-                      className="max-w-full sm:max-w-xs"
-                      onValueChange={setSelectedNames}
-                      placeholder="Select Salespeople..."
-                    >
-                      {salesPeople.map((item) => (
-                        <MultiSelectItem key={item.name} value={item.name}>
-                          {item.name}
-                        </MultiSelectItem>
-                      ))}
-                    </MultiSelect>
-                    <Select
-                      className="max-w-full sm:max-w-xs"
-                      defaultValue="all"
-                      onValueChange={setSelectedStatus}
-                    >
-                      <SelectItem value="all">All Performances</SelectItem>
-                      <SelectItem value="overperforming">
-                        Overperforming
-                      </SelectItem>
-                      <SelectItem value="average">Average</SelectItem>
-                      <SelectItem value="underperforming">
-                        Underperforming
-                      </SelectItem>
-                    </Select>
-                  </div>
-                  <Table className="mt-6">
-                    <TableHead>
-                      <TableRow>
-                        <TableHeaderCell>Name</TableHeaderCell>
-                        <TableHeaderCell className="text-right">
-                          Leads
-                        </TableHeaderCell>
-                        <TableHeaderCell className="text-right">
-                          Sales ($)
-                        </TableHeaderCell>
-                        <TableHeaderCell className="text-right">
-                          Quota ($)
-                        </TableHeaderCell>
-                        <TableHeaderCell className="text-right">
-                          Variance
-                        </TableHeaderCell>
-                        <TableHeaderCell className="text-right">
-                          Region
-                        </TableHeaderCell>
-                        <TableHeaderCell className="text-right">
-                          Status
-                        </TableHeaderCell>
-                      </TableRow>
-                    </TableHead>
-
-                    <TableBody>
-                      {salesPeople
-                        .filter((item) => isSalesPersonSelected(item))
-                        .map((item) => (
-                          <TableRow key={item.name}>
-                            <TableCell>{item.name}</TableCell>
-                            <TableCell className="text-right">
-                              {item.leads}
-                            </TableCell>
-                            <TableCell className="text-right">
-                              {item.sales}
-                            </TableCell>
-                            <TableCell className="text-right">
-                              {item.quota}
-                            </TableCell>
-                            <TableCell className="text-right">
-                              {item.variance}
-                            </TableCell>
-                            <TableCell className="text-right">
-                              {item.region}
-                            </TableCell>
-                            <TableCell className="text-right">
-                              <BadgeDelta
-                                deltaType={deltaTypes[item.status]}
-                                size="xs"
-                              >
-                                {item.status}
-                              </BadgeDelta>
-                            </TableCell>
-                          </TableRow>
+                        {salesPeople.map((item) => (
+                          <MultiSelectItem key={item.name} value={item.name}>
+                            {item.name}
+                          </MultiSelectItem>
                         ))}
-                    </TableBody>
-                  </Table>
-                </>
-              </Card>
-            </div>
-          </TabPanel>
-        </TabPanels>
+                      </MultiSelect>
+                      <Select
+                        className="max-w-full sm:max-w-xs"
+                        defaultValue="all"
+                        onValueChange={setSelectedStatus}
+                      >
+                        <SelectItem value="all">All Performances</SelectItem>
+                        <SelectItem value="overperforming">
+                          Overperforming
+                        </SelectItem>
+                        <SelectItem value="average">Average</SelectItem>
+                        <SelectItem value="underperforming">
+                          Underperforming
+                        </SelectItem>
+                      </Select>
+                    </div>
+                    <Table className="mt-6">
+                      <TableHead>
+                        <TableRow>
+                          <TableHeaderCell>Name</TableHeaderCell>
+                          <TableHeaderCell className="text-right">
+                            Leads
+                          </TableHeaderCell>
+                          <TableHeaderCell className="text-right">
+                            Sales ($)
+                          </TableHeaderCell>
+                          <TableHeaderCell className="text-right">
+                            Quota ($)
+                          </TableHeaderCell>
+                          <TableHeaderCell className="text-right">
+                            Variance
+                          </TableHeaderCell>
+                          <TableHeaderCell className="text-right">
+                            Region
+                          </TableHeaderCell>
+                          <TableHeaderCell className="text-right">
+                            Status
+                          </TableHeaderCell>
+                        </TableRow>
+                      </TableHead>
+
+                      <TableBody>
+                        {salesPeople
+                          .filter((item) => isSalesPersonSelected(item))
+                          .map((item) => (
+                            <TableRow key={item.name}>
+                              <TableCell>{item.name}</TableCell>
+                              <TableCell className="text-right">
+                                {item.leads}
+                              </TableCell>
+                              <TableCell className="text-right">
+                                {item.sales}
+                              </TableCell>
+                              <TableCell className="text-right">
+                                {item.quota}
+                              </TableCell>
+                              <TableCell className="text-right">
+                                {item.variance}
+                              </TableCell>
+                              <TableCell className="text-right">
+                                {item.region}
+                              </TableCell>
+                              <TableCell className="text-right">
+                                <BadgeDelta
+                                  deltaType={deltaTypes[item.status]}
+                                  size="xs"
+                                >
+                                  {item.status}
+                                </BadgeDelta>
+                              </TableCell>
+                            </TableRow>
+                          ))}
+                      </TableBody>
+                    </Table>
+                  </>
+                </Card>
+              </div>
+            </TabPanel>
+          </TabPanels>
+        </TabGroup>
+      </main>
+      <TabGroup className="sticky-bottom">
+        <TabList className="flex w-full span-center" variant="solid">
+          <Tab className="flex-grow text-center">Detail</Tab>
+          <Tab className="flex-grow text-center">Detail</Tab>
+          <Tab className="flex-grow text-center">Detail</Tab>
+          <Tab className="flex-grow text-center">Detail</Tab>
+        </TabList>
       </TabGroup>
-    </main>
+    </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 /* eslint-disable max-len */
 const colors = require("tailwindcss/colors");
 module.exports = {
+  darkMode: "class",
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -16,31 +17,31 @@ module.exports = {
         // light mode
         tremor: {
           brand: {
-            faint: "#0B1229",
-            muted: colors.blue[950],
-            subtle: colors.blue[800],
+            faint: colors.blue[50],
+            muted: colors.blue[200],
+            subtle: colors.blue[400],
             DEFAULT: colors.blue[500],
-            emphasis: colors.blue[400],
-            inverted: colors.gray[950],
+            emphasis: colors.blue[700],
+            inverted: colors.white,
           },
           background: {
-            muted: "#131A2B",
-            subtle: colors.gray[800],
-            DEFAULT: colors.gray[900],
-            emphasis: colors.gray[300],
+            muted: colors.gray[50],
+            subtle: colors.gray[100],
+            DEFAULT: colors.white,
+            emphasis: colors.gray[700],
           },
           border: {
-            DEFAULT: colors.gray[800],
+            DEFAULT: colors.gray[200],
           },
           ring: {
-            DEFAULT: colors.gray[800],
+            DEFAULT: colors.gray[200],
           },
           content: {
-            subtle: colors.gray[600],
-            DEFAULT: colors.gray[600],
-            emphasis: colors.gray[200],
-            strong: colors.gray[50],
-            inverted: colors.black,
+            subtle: colors.gray[400],
+            DEFAULT: colors.gray[500],
+            emphasis: colors.gray[700],
+            strong: colors.gray[900],
+            inverted: colors.white,
           },
         },
         // dark mode
