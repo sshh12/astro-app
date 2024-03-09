ce8963b480709d4de8183884e69a23231c81582f
Shrivu Shankar <shrivu1122@gmail.com>
2024-03-09 15:17:27 -0800

    update schema

--- modal/modal_base.py
+++ modal/modal_base.py
@@ -5,7 +5,7 @@ image_base = (
     .pip_install("skyfield==1.48", "prisma==0.12.0", "pytz==2024.1", "modal~=0.61.0")
     .apt_install("curl")
     .run_commands(
-        "curl https://raw.githubusercontent.com/sshh12/astro-app/main/prisma/schema.prisma?51 > /root/schema.prisma",
+        "curl https://raw.githubusercontent.com/sshh12/astro-app/main/prisma/schema.prisma?52 > /root/schema.prisma",
         "prisma generate --schema /root/schema.prisma",
         "python -c \"from skyfield.api import Loader; Loader('/root/cache/')('de421.bsp')\"",
     )
