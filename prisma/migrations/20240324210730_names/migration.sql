a92888e5bd122104090ff7d2f69291fa21f88edf
Shrivu Shankar <shrivu1122@gmail.com>
2024-03-24 16:30:45 -0700

    migrate

---
+++ prisma/migrations/20240324210730_names/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "Equipment" ADD COLUMN     "binoName" STRING;
+ALTER TABLE "Equipment" ADD COLUMN     "camName" STRING;
+ALTER TABLE "Equipment" ADD COLUMN     "teleName" STRING;
---
+++ prisma/migrations/20240324220041_camname/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Equipment" ADD COLUMN     "eyeName" STRING;
